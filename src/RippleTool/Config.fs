16782da5730cadfa0cbc126f157e4d5744fecb9a
Jim Killingsworth <jkillingsworth@gmail.com>
2016-01-14 01:23:39 -0800

    Rename setting rippleKey to accountId

--- src/RippleTool/Config.fs
+++ src/RippleTool/Config.fs
@@ -7,5 +7,5 @@ open System.Configuration
 let private reader = AppSettingsReader()
 
 let serverUri = reader.GetValue("serverUri", typeof<string>) :?> string
-let rippleKey = reader.GetValue("rippleKey", typeof<string>) :?> string
 let secretKey = reader.GetValue("secretKey", typeof<string>) :?> string
+let accountId = reader.GetValue("accountId", typeof<string>) :?> string
