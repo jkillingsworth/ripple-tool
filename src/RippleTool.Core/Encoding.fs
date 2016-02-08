a2fb8f3c9fbd641aeaa6508d8649d9bf5d93becd
Jim Killingsworth <jkillingsworth@gmail.com>
2016-02-07 19:25:34 -0800

    Get set up for unit testing

---
+++ src/RippleTool.Core/Encoding.fs
@@ -0,0 +1,19 @@
+﻿module RippleTool.Encoding
+
+open System
+
+//-------------------------------------------------------------------------------------------------
+
+module Hex =
+
+    let stringToBinary (input : string) =
+        input
+        |> Array.ofSeq
+        |> Array.chunkBySize 2
+        |> Array.map String
+        |> Array.map (fun x -> Byte.Parse(x, Globalization.NumberStyles.AllowHexSpecifier))
+
+    let binaryToString (input : byte[]) =
+        input
+        |> Array.map (sprintf "%02X")
+        |> String.Concat
---
+++ tests/RippleTool.Core.Tests/Encoding.fs
@@ -0,0 +1,23 @@
+﻿module RippleTool.Tests.Encoding
+
+open NUnit.Framework
+open FsUnit
+open RippleTool.Encoding
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Hex, string to binary`` () =
+
+    let input = "466F6F626172"
+    let expected = [| 0x46uy; 0x6Fuy; 0x6Fuy; 0x62uy; 0x61uy; 0x72uy |]
+
+    input |> Hex.stringToBinary |> should equal expected
+
+[<Test>]
+let ``Hex, binary to string`` () =
+
+    let input = [| 0x46uy; 0x6Fuy; 0x6Fuy; 0x62uy; 0x61uy; 0x72uy |]
+    let expected = "466F6F626172"
+
+    input |> Hex.binaryToString |> should equal expected
