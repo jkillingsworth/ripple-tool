49af6047e908d9bb78f0cad418ce55ab791781dc
Jim Killingsworth <jkillingsworth@gmail.com>
2016-02-08 22:04:52 -0800

    Create new encoding module for integers, hexadecimal, and base 58 numbers

--- src/RippleTool.Core/Encoding.fs
+++ src/RippleTool.Core/Encoding.fs
@@ -1,19 +1,177 @@
 ﻿module RippleTool.Encoding
 
 open System
+open System.Security.Cryptography
 
 //-------------------------------------------------------------------------------------------------
 
-module Hex =
+module Binary =
 
-    let stringToBinary (input : string) =
+    let private reverseIfPlatformIsNotBigEndian binary =
+        if BitConverter.IsLittleEndian then
+            binary |> Array.rev
+        else
+            binary
+
+    let private removeSignIndicator (binary : byte[]) =
+        if (binary.[0] = 0uy && binary.Length > 1) then
+            binary.[1 ..]
+        else
+            binary
+
+    let private appendSignIndicator (binary : byte[]) =
+        if (binary.[0] > 127uy) then
+            binary |> Array.append [| 0uy |]
+        else
+            binary
+
+    let private convertWith conversion binary = conversion (binary, 0)
+
+    let ofBigint (input : bigint) =
+        input.ToByteArray()
+        |> reverseIfPlatformIsNotBigEndian
+        |> removeSignIndicator
+
+    let toBigint (input : byte[]) =
+        input
+        |> appendSignIndicator
+        |> reverseIfPlatformIsNotBigEndian
+        |> bigint
+
+    let ofUint16 (input : uint16) =
+        input
+        |> BitConverter.GetBytes
+        |> reverseIfPlatformIsNotBigEndian
+
+    let toUint16 (input : byte[]) =
+        input
+        |> reverseIfPlatformIsNotBigEndian
+        |> convertWith BitConverter.ToUInt16
+
+    let ofUint32 (input : uint32) =
+        input
+        |> BitConverter.GetBytes
+        |> reverseIfPlatformIsNotBigEndian
+
+    let toUint32 (input : byte[]) =
+        input
+        |> reverseIfPlatformIsNotBigEndian
+        |> convertWith BitConverter.ToUInt32
+
+    let ofUint64 (input : uint64) =
+        input
+        |> BitConverter.GetBytes
+        |> reverseIfPlatformIsNotBigEndian
+
+    let toUint64 (input : byte[]) =
+        input
+        |> reverseIfPlatformIsNotBigEndian
+        |> convertWith BitConverter.ToUInt64
+
+    let ofHex (input : string) =
         input
         |> Array.ofSeq
         |> Array.chunkBySize 2
         |> Array.map String
         |> Array.map (fun x -> Byte.Parse(x, Globalization.NumberStyles.AllowHexSpecifier))
 
-    let binaryToString (input : byte[]) =
+    let toHex (input : byte[]) =
         input
         |> Array.map (sprintf "%02X")
         |> String.Concat
+
+//-------------------------------------------------------------------------------------------------
+
+module Base58 =
+
+    let alphabet = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz"
+
+    type private Version =
+        | AccountId = 00uy
+        | SecretKey = 33uy
+
+    let private computeHashsum version payload =
+        use sha256 = SHA256.Create()
+        payload
+        |> Array.append version
+        |> sha256.ComputeHash
+        |> sha256.ComputeHash
+        |> Array.take 4
+
+    let ofBigint (input : bigint) =
+
+        let generator = function
+            | state when state = 0I -> None
+            | state ->
+                let modulo = state % 58I
+                let divide = state / 58I
+                Some (modulo, divide)
+
+        let includeZeroPlaceholder chars =
+            if Array.isEmpty chars then [| alphabet.[0] |] else chars
+
+        input
+        |> Array.unfold generator
+        |> Array.map (fun x -> alphabet.[int x])
+        |> Array.rev
+        |> includeZeroPlaceholder
+        |> String
+
+    let toBigint (input : string) =
+        input
+        |> Seq.map (fun x -> alphabet.IndexOf(x))
+        |> Seq.fold (fun acc x -> (acc * 58I) + bigint x) 0I
+
+    let private encode (version : Version) (input : byte[]) =
+
+        let binaryVersion = [| byte version |]
+        let binaryPayload = input
+        let binaryHashsum = computeHashsum binaryVersion binaryPayload
+
+        let binary = Array.concat [ binaryVersion; binaryPayload; binaryHashsum ]
+
+        let encodedPadding =
+            binary
+            |> Array.takeWhile (fun x -> x = 0x00uy)
+            |> Array.map (fun _ -> alphabet.[0])
+            |> String
+
+        let encodedInteger =
+            binary
+            |> Binary.toBigint
+            |> ofBigint
+
+        encodedPadding + encodedInteger
+
+    let private decode (version : Version) (input : string) =
+
+        let decodedPadding =
+            input
+            |> Seq.takeWhile (fun x -> x = alphabet.[0])
+            |> Seq.length
+            |> Array.zeroCreate<byte>
+
+        let decodedInteger =
+            input
+            |> toBigint
+            |> Binary.ofBigint
+
+        let binary = Array.concat [ decodedPadding; decodedInteger ]
+        let length = binary.Length - 4
+
+        let binaryVersion = binary.[0 .. 0]
+        let binaryPayload = binary.[1 .. length - 1]
+        let binaryHashsum = binary.[length ..]
+
+        if binaryVersion <> [| byte version |]
+            then failwith "Invalid version."
+
+        if binaryHashsum <> computeHashsum binaryVersion binaryPayload
+            then failwith "Invalid hashsum."
+
+        binaryPayload
+
+    let encodeAccountId = encode Version.AccountId
+    let encodeSecretKey = encode Version.SecretKey
+    let decodeAccountId = decode Version.AccountId
+    let decodeSecretKey = decode Version.SecretKey
--- tests/RippleTool.Core.Tests/Encoding.fs
+++ tests/RippleTool.Core.Tests/Encoding.fs
@@ -7,17 +7,345 @@ open RippleTool.Encoding
 //-------------------------------------------------------------------------------------------------
 
 [<Test>]
-let ``Hex, string to binary`` () =
+let ``Binary of bigint 000`` () =
+    000I |> Binary.ofBigint |> should equal [| 0x00uy |]
+
+[<Test>]
+let ``Binary of bigint 001`` () =
+    001I |> Binary.ofBigint |> should equal [| 0x01uy |]
+
+[<Test>]
+let ``Binary of bigint 127`` () =
+    127I |> Binary.ofBigint |> should equal [| 0x7Fuy |]
+
+[<Test>]
+let ``Binary of bigint 128`` () =
+    128I |> Binary.ofBigint |> should equal [| 0x80uy |]
+
+[<Test>]
+let ``Binary of bigint 255`` () =
+    255I |> Binary.ofBigint |> should equal [| 0xFFuy |]
+
+[<Test>]
+let ``Binary of bigint 256`` () =
+    256I |> Binary.ofBigint |> should equal [| 0x01uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of bigint 999`` () =
+    999I |> Binary.ofBigint |> should equal [| 0x03uy; 0xE7uy |]
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary to bigint 000`` () =
+    [| 0x00uy |] |> Binary.toBigint |> should equal 000I
+
+[<Test>]
+let ``Binary to bigint 001`` () =
+    [| 0x01uy |] |> Binary.toBigint |> should equal 001I
+
+[<Test>]
+let ``Binary to bigint 127`` () =
+    [| 0x7Fuy |] |> Binary.toBigint |> should equal 127I
+
+[<Test>]
+let ``Binary to bigint 128`` () =
+    [| 0x80uy |] |> Binary.toBigint |> should equal 128I
+
+[<Test>]
+let ``Binary to bigint 255`` () =
+    [| 0xFFuy |] |> Binary.toBigint |> should equal 255I
+
+[<Test>]
+let ``Binary to bigint 256`` () =
+    [| 0x01uy; 0x00uy |] |> Binary.toBigint |> should equal 256I
+
+[<Test>]
+let ``Binary to bigint 999`` () =
+    [| 0x03uy; 0xE7uy |] |> Binary.toBigint |> should equal 999I
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary of uint16 000`` () =
+    000us |> Binary.ofUint16 |> should equal [| 0x00uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of uint16 001`` () =
+    001us |> Binary.ofUint16 |> should equal [| 0x00uy; 0x01uy |]
+
+[<Test>]
+let ``Binary of uint16 255`` () =
+    255us |> Binary.ofUint16 |> should equal [| 0x00uy; 0xFFuy |]
+
+[<Test>]
+let ``Binary of uint16 256`` () =
+    256us |> Binary.ofUint16 |> should equal [| 0x01uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of uint16 999`` () =
+    999us |> Binary.ofUint16 |> should equal [| 0x03uy; 0xE7uy |]
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary to uint16 000`` () =
+    [| 0x00uy; 0x00uy |] |> Binary.toUint16 |> should equal 000us
+
+[<Test>]
+let ``Binary to uint16 001`` () =
+    [| 0x00uy; 0x01uy |] |> Binary.toUint16 |> should equal 001us
+
+[<Test>]
+let ``Binary to uint16 255`` () =
+    [| 0x00uy; 0xFFuy |] |> Binary.toUint16 |> should equal 255us
+
+[<Test>]
+let ``Binary to uint16 256`` () =
+    [| 0x01uy; 0x00uy |] |> Binary.toUint16 |> should equal 256us
+
+[<Test>]
+let ``Binary to uint16 999`` () =
+    [| 0x03uy; 0xE7uy |] |> Binary.toUint16 |> should equal 999us
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary of uint32 000`` () =
+    000u |> Binary.ofUint32 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of uint32 001`` () =
+    001u |> Binary.ofUint32 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x01uy |]
+
+[<Test>]
+let ``Binary of uint32 255`` () =
+    255u |> Binary.ofUint32 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0xFFuy |]
+
+[<Test>]
+let ``Binary of uint32 256`` () =
+    256u |> Binary.ofUint32 |> should equal [| 0x00uy; 0x00uy; 0x01uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of uint32 999`` () =
+    999u |> Binary.ofUint32 |> should equal [| 0x00uy; 0x00uy; 0x03uy; 0xE7uy |]
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary to uint32 000`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x00uy |] |> Binary.toUint32 |> should equal 000u
+
+[<Test>]
+let ``Binary to uint32 001`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x01uy |] |> Binary.toUint32 |> should equal 001u
+
+[<Test>]
+let ``Binary to uint32 255`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0xFFuy |] |> Binary.toUint32 |> should equal 255u
+
+[<Test>]
+let ``Binary to uint32 256`` () =
+    [| 0x00uy; 0x00uy; 0x01uy; 0x00uy |] |> Binary.toUint32 |> should equal 256u
+
+[<Test>]
+let ``Binary to uint32 999`` () =
+    [| 0x00uy; 0x00uy; 0x03uy; 0xE7uy |] |> Binary.toUint32 |> should equal 999u
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary of uint64 000`` () =
+    000UL |> Binary.ofUint64 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of uint64 001`` () =
+    001UL |> Binary.ofUint64 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x01uy |]
+
+[<Test>]
+let ``Binary of uint64 255`` () =
+    255UL |> Binary.ofUint64 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0xFFuy |]
+
+[<Test>]
+let ``Binary of uint64 256`` () =
+    256UL |> Binary.ofUint64 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x01uy; 0x00uy |]
+
+[<Test>]
+let ``Binary of uint64 999`` () =
+    999UL |> Binary.ofUint64 |> should equal [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x03uy; 0xE7uy |]
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary to uint64 000`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy |] |> Binary.toUint64 |> should equal 000UL
+
+[<Test>]
+let ``Binary to uint64 001`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x01uy |] |> Binary.toUint64 |> should equal 001UL
+
+[<Test>]
+let ``Binary to uint64 255`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0xFFuy |] |> Binary.toUint64 |> should equal 255UL
+
+[<Test>]
+let ``Binary to uint64 256`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x01uy; 0x00uy |] |> Binary.toUint64 |> should equal 256UL
+
+[<Test>]
+let ``Binary to uint64 999`` () =
+    [| 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x00uy; 0x03uy; 0xE7uy |] |> Binary.toUint64 |> should equal 999UL
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Binary of hex string`` () =
 
     let input = "466F6F626172"
     let expected = [| 0x46uy; 0x6Fuy; 0x6Fuy; 0x62uy; 0x61uy; 0x72uy |]
 
-    input |> Hex.stringToBinary |> should equal expected
+    input |> Binary.ofHex |> should equal expected
 
 [<Test>]
-let ``Hex, binary to string`` () =
+let ``Binary to hex string`` () =
 
     let input = [| 0x46uy; 0x6Fuy; 0x6Fuy; 0x62uy; 0x61uy; 0x72uy |]
     let expected = "466F6F626172"
 
-    input |> Hex.binaryToString |> should equal expected
+    input |> Binary.toHex |> should equal expected
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Base58 of bigint 00`` () =
+    00I |> Base58.ofBigint |> should equal "r"
+
+[<Test>]
+let ``Base58 of bigint 01`` () =
+    01I |> Base58.ofBigint |> should equal "p"
+
+[<Test>]
+let ``Base58 of bigint 02`` () =
+    02I |> Base58.ofBigint |> should equal "s"
+
+[<Test>]
+let ``Base58 of bigint 55`` () =
+    55I |> Base58.ofBigint |> should equal "x"
+
+[<Test>]
+let ``Base58 of bigint 56`` () =
+    56I |> Base58.ofBigint |> should equal "y"
+
+[<Test>]
+let ``Base58 of bigint 57`` () =
+    57I |> Base58.ofBigint |> should equal "z"
+
+[<Test>]
+let ``Base58 of bigint 58`` () =
+    58I |> Base58.ofBigint |> should equal "pr"
+
+[<Test>]
+let ``Base58 of bigint 59`` () =
+    59I |> Base58.ofBigint |> should equal "pp"
+
+[<Test>]
+let ``Base58 of bigint 60`` () =
+    60I |> Base58.ofBigint |> should equal "ps"
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Base58 to bigint 00`` () =
+    "r" |> Base58.toBigint |> should equal 00I
+
+[<Test>]
+let ``Base58 to bigint 01`` () =
+    "p" |> Base58.toBigint |> should equal 01I
+
+[<Test>]
+let ``Base58 to bigint 02`` () =
+    "s" |> Base58.toBigint |> should equal 02I
+
+[<Test>]
+let ``Base58 to bigint 55`` () =
+    "x" |> Base58.toBigint |> should equal 55I
+
+[<Test>]
+let ``Base58 to bigint 56`` () =
+    "y" |> Base58.toBigint |> should equal 56I
+
+[<Test>]
+let ``Base58 to bigint 57`` () =
+    "z" |> Base58.toBigint |> should equal 57I
+
+[<Test>]
+let ``Base58 to bigint 58`` () =
+    "pr" |> Base58.toBigint |> should equal 58I
+
+[<Test>]
+let ``Base58 to bigint 59`` () =
+    "pp" |> Base58.toBigint |> should equal 59I
+
+[<Test>]
+let ``Base58 to bigint 60`` () =
+    "ps" |> Base58.toBigint |> should equal 60I
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Base58 encode account id rrrrrrrrrrrrrrrrrrrrrhoLvTp`` () =
+
+    let input = Binary.ofHex "0000000000000000000000000000000000000000"
+    let expected = "rrrrrrrrrrrrrrrrrrrrrhoLvTp"
+    input |> Base58.encodeAccountId |> should equal expected
+
+[<Test>]
+let ``Base58 encode account id rrrrrrrrrrrrrrrrrrrrBZbvji`` () =
+
+    let input = Binary.ofHex "0000000000000000000000000000000000000001"
+    let expected = "rrrrrrrrrrrrrrrrrrrrBZbvji"
+    input |> Base58.encodeAccountId |> should equal expected
+
+[<Test>]
+let ``Base58 encode account id rhcfR9Cg98qCxHpCcPBmMonbDBXo84wyTn`` () =
+
+    let input = Binary.ofHex "278C4AC8FEAB993E85BBA8FF8401E7C37E062C56"
+    let expected = "rhcfR9Cg98qCxHpCcPBmMonbDBXo84wyTn"
+    input |> Base58.encodeAccountId |> should equal expected
+
+[<Test>]
+let ``Base58 encode secret key shHM53KPZ87Gwdqarm1bAmPeXg8Tn`` () =
+
+    let input = Binary.ofHex "71ED064155FFADFA38782C5E0158CB26"
+    let expected = "shHM53KPZ87Gwdqarm1bAmPeXg8Tn"
+    input |> Base58.encodeSecretKey |> should equal expected
+
+//-------------------------------------------------------------------------------------------------
+
+[<Test>]
+let ``Base58 decode account id rrrrrrrrrrrrrrrrrrrrrhoLvTp`` () =
+
+    let input = "rrrrrrrrrrrrrrrrrrrrrhoLvTp"
+    let expected = Binary.ofHex "0000000000000000000000000000000000000000"
+    input |> Base58.decodeAccountId |> should equal expected
+
+[<Test>]
+let ``Base58 decode account id rrrrrrrrrrrrrrrrrrrrBZbvji`` () =
+
+    let input = "rrrrrrrrrrrrrrrrrrrrBZbvji"
+    let expected = Binary.ofHex "0000000000000000000000000000000000000001"
+    input |> Base58.decodeAccountId |> should equal expected
+
+[<Test>]
+let ``Base58 decode account id rhcfR9Cg98qCxHpCcPBmMonbDBXo84wyTn`` () =
+
+    let input = "rhcfR9Cg98qCxHpCcPBmMonbDBXo84wyTn"
+    let expected = Binary.ofHex "278C4AC8FEAB993E85BBA8FF8401E7C37E062C56"
+    input |> Base58.decodeAccountId |> should equal expected
+
+[<Test>]
+let ``Base58 decode secret key shHM53KPZ87Gwdqarm1bAmPeXg8Tn`` () =
+
+    let input = "shHM53KPZ87Gwdqarm1bAmPeXg8Tn"
+    let expected = Binary.ofHex "71ED064155FFADFA38782C5E0158CB26"
+    input |> Base58.decodeSecretKey |> should equal expected
