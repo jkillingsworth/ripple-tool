1f70519dab4eccb594e2da7474d6bb956e1c1360
Jim Killingsworth <jkillingsworth@gmail.com>
2016-02-28 21:22:55 -0800

    Refactor view models

--- src/RippleTool.Core/UI.Models.fs
+++ src/RippleTool.Core/UI.Models.fs
@@ -9,18 +9,12 @@ open RippleTool.Integration
 
 //-------------------------------------------------------------------------------------------------
 
-let private propertyName = function
-    | PropertyGet (_, info, _) -> info.Name
-    | _ -> failwith "Expecting a property expression."
-
-let private optional value map =
-    match value with
-    | value when value = "" -> None
-    | value -> value |> map |> Some
-
-//-------------------------------------------------------------------------------------------------
-
+[<AbstractClass>]
 type Model() =
+
+    let propertyName = function
+        | PropertyGet (_, info, _) -> info.Name
+        | _ -> failwith "Expecting a property expression."
 
     let propertyChanged = Event<_, _>()
 
@@ -36,103 +30,104 @@ type Model() =
 
 //-------------------------------------------------------------------------------------------------
 
+let private optional value map =
+    match value with
+    | value when value = "" -> None
+    | value -> value |> map |> Some
+
+let private toCurrency code issuer = function
+    | true  -> IssuedCurrency <| { Code = code; Issuer = issuer }
+    | false -> NativeCurrency <| NativeCurrency.NativeCurrency
+
+let private toAmount value currency issuer = function
+    | true  -> IssuedAmount <| { Value = Decimal.Parse value; Currency = currency; Issuer = issuer }
+    | false -> NativeAmount <| Decimal.Parse value
+
+let private set (this : Model) value field expr =
+    field := value
+    this.OnPropertyChanged(expr)
+
+//-------------------------------------------------------------------------------------------------
+
 type BookOffersModel() =
 
     inherit Model()
 
-    let mutable taker = ""
-    let mutable limit = ""
-    let mutable takerGetsIssued = true
-    let mutable takerGetsNative = false
-    let mutable takerGetsCurrency = ""
-    let mutable takerGetsIssuer = ""
-    let mutable takerPaysIssued = true
-    let mutable takerPaysNative = false
-    let mutable takerPaysCurrency = ""
-    let mutable takerPaysIssuer = ""
+    let taker = ref ""
+    let limit = ref ""
+    let takerGetsIssued = ref true
+    let takerGetsNative = ref false
+    let takerGetsCurrency = ref ""
+    let takerGetsIssuer = ref ""
+    let takerPaysIssued = ref true
+    let takerPaysNative = ref false
+    let takerPaysCurrency = ref ""
+    let takerPaysIssuer = ref ""
 
     member this.Taker
-        with get () = taker
-        and set value =
-            taker <- value
-            this.OnPropertyChanged(<@ this.Taker @>)
+        with get () = !taker
+        and set value = set this value taker <@ this.Taker @>
 
     member this.Limit
-        with get () = limit
-        and set value =
-            limit <- value
-            this.OnPropertyChanged(<@ this.Limit @>)
+        with get () = !limit
+        and set value = set this value limit <@ this.Limit @>
 
     member this.TakerGetsIssued
-        with get () = takerGetsIssued
+        with get () = !takerGetsIssued
         and set value =
-            takerGetsIssued <- value
-            takerGetsNative <- not value
-            this.OnPropertyChanged(<@ this.TakerGetsIssued @>)
-            this.OnPropertyChanged(<@ this.TakerGetsNative @>)
+            let issued, native = (value, not value)
+            set this issued takerGetsIssued <@ this.TakerGetsIssued @>
+            set this native takerGetsNative <@ this.TakerGetsNative @>
 
     member this.TakerGetsNative
-        with get () = takerGetsNative
+        with get () = !takerGetsNative
         and set value =
-            takerGetsIssued <- not value
-            takerGetsNative <- value
-            this.OnPropertyChanged(<@ this.TakerGetsIssued @>)
-            this.OnPropertyChanged(<@ this.TakerGetsNative @>)
+            let issued, native = (not value, value)
+            set this issued takerGetsIssued <@ this.TakerGetsIssued @>
+            set this native takerGetsNative <@ this.TakerGetsNative @>
 
     member this.TakerGetsCurrency
-        with get () = takerGetsCurrency
-        and set value =
-            takerGetsCurrency <- value
-            this.OnPropertyChanged(<@ this.TakerGetsCurrency @>)
+        with get () = !takerGetsCurrency
+        and set value = set this value takerGetsCurrency <@ this.TakerGetsCurrency @>
 
     member this.TakerGetsIssuer
-        with get () = takerGetsIssuer
-        and set value =
-            takerGetsIssuer <- value
-            this.OnPropertyChanged(<@ this.TakerGetsIssuer @>)
+        with get () = !takerGetsIssuer
+        and set value = set this value takerGetsIssuer <@ this.TakerGetsIssuer @>
 
     member this.TakerPaysIssued
-        with get () = takerPaysIssued
+        with get () = !takerPaysIssued
         and set value =
-            takerPaysIssued <- value
-            takerPaysNative <- not value
-            this.OnPropertyChanged(<@ this.TakerPaysIssued @>)
-            this.OnPropertyChanged(<@ this.TakerPaysNative @>)
+            let issued, native = (value, not value)
+            set this issued takerPaysIssued <@ this.TakerPaysIssued @>
+            set this native takerPaysNative <@ this.TakerPaysNative @>
 
     member this.TakerPaysNative
-        with get () = takerPaysNative
+        with get () = !takerPaysNative
         and set value =
-            takerPaysIssued <- not value
-            takerPaysNative <- value
-            this.OnPropertyChanged(<@ this.TakerPaysIssued @>)
-            this.OnPropertyChanged(<@ this.TakerPaysNative @>)
+            let issued, native = (not value, value)
+            set this issued takerPaysIssued <@ this.TakerPaysIssued @>
+            set this native takerPaysNative <@ this.TakerPaysNative @>
 
     member this.TakerPaysCurrency
-        with get () = takerPaysCurrency
-        and set value =
-            takerPaysCurrency <- value
-            this.OnPropertyChanged(<@ this.TakerPaysCurrency @>)
+        with get () = !takerPaysCurrency
+        and set value = set this value takerPaysCurrency <@ this.TakerPaysCurrency @>
 
     member this.TakerPaysIssuer
-        with get () = takerPaysIssuer
-        and set value =
-            takerPaysIssuer <- value
-            this.OnPropertyChanged(<@ this.TakerPaysIssuer @>)
+        with get () = !takerPaysIssuer
+        and set value = set this value takerPaysIssuer <@ this.TakerPaysIssuer @>
 
     member this.Submit() =
 
-        let taker = optional taker id
-        let limit = optional limit UInt32.Parse
+        let taker = optional !taker id
+        let limit = optional !limit UInt32.Parse
 
         let takerGets =
-            match takerGetsIssued with
-            | true  -> IssuedCurrency { Code = takerGetsCurrency; Issuer = takerGetsIssuer }
-            | false -> NativeCurrency NativeCurrency.NativeCurrency
+            !takerGetsIssued
+            |> toCurrency !takerGetsCurrency !takerGetsIssuer
 
         let takerPays =
-            match takerPaysIssued with
-            | true  -> IssuedCurrency { Code = takerPaysCurrency; Issuer = takerPaysIssuer }
-            | false -> NativeCurrency NativeCurrency.NativeCurrency
+            !takerPaysIssued
+            |> toCurrency !takerPaysCurrency !takerPaysIssuer
 
         let command =
             { Taker = taker
@@ -148,74 +143,57 @@ type RipplePathFindModel() =
 
     inherit Model()
 
-    let mutable sourceAccount = ""
-    let mutable destinationAccount = ""
-    let mutable destinationAmountIssued = true
-    let mutable destinationAmountNative = false
-    let mutable destinationAmountValue = ""
-    let mutable destinationAmountCurrency = ""
-    let mutable destinationAmountIssuer = ""
+    let sourceAccount = ref ""
+    let destinationAccount = ref ""
+    let destinationAmountIssued = ref true
+    let destinationAmountNative = ref false
+    let destinationAmountValue = ref ""
+    let destinationAmountCurrency = ref ""
+    let destinationAmountIssuer = ref ""
 
     member this.SourceAccount
-        with get () = sourceAccount
-        and set value =
-            sourceAccount <- value
-            this.OnPropertyChanged(<@ this.SourceAccount @>)
+        with get () = !sourceAccount
+        and set value = set this value sourceAccount <@ this.SourceAccount @>
 
     member this.DestinationAccount
-        with get () = destinationAccount
-        and set value =
-            destinationAccount <- value
-            this.OnPropertyChanged(<@ this.DestinationAccount @>)
+        with get () = !destinationAccount
+        and set value = set this value destinationAccount <@ this.DestinationAccount @>
 
     member this.DestinationAmountIssued
-        with get () = destinationAmountIssued
+        with get () = !destinationAmountIssued
         and set value =
-            destinationAmountIssued <- value
-            destinationAmountNative <- not value
-            this.OnPropertyChanged(<@ this.DestinationAmountIssued @>)
-            this.OnPropertyChanged(<@ this.DestinationAmountNative @>)
+            let issued, native = (value, not value)
+            set this issued destinationAmountIssued <@ this.DestinationAmountIssued @>
+            set this native destinationAmountNative <@ this.DestinationAmountNative @>
 
     member this.DestinationAmountNative
-        with get () = destinationAmountNative
+        with get () = !destinationAmountNative
         and set value =
-            destinationAmountIssued <- not value
-            destinationAmountNative <- value
-            this.OnPropertyChanged(<@ this.DestinationAmountIssued @>)
-            this.OnPropertyChanged(<@ this.DestinationAmountNative @>)
+            let issued, native = (not value, value)
+            set this issued destinationAmountIssued <@ this.DestinationAmountIssued @>
+            set this native destinationAmountNative <@ this.DestinationAmountNative @>
 
     member this.DestinationAmountValue
-        with get () = destinationAmountValue
-        and set value =
-            destinationAmountValue <- value
-            this.OnPropertyChanged(<@ this.DestinationAmountValue @>)
+        with get () = !destinationAmountValue
+        and set value = set this value destinationAmountValue <@ this.DestinationAmountValue @>
 
     member this.DestinationAmountCurrency
-        with get () = destinationAmountCurrency
-        and set value =
-            destinationAmountCurrency <- value
-            this.OnPropertyChanged(<@ this.DestinationAmountCurrency @>)
+        with get () = !destinationAmountCurrency
+        and set value = set this value destinationAmountCurrency <@ this.DestinationAmountCurrency @>
 
     member this.DestinationAmountIssuer
-        with get () = destinationAmountIssuer
-        and set value =
-            destinationAmountIssuer <- value
-            this.OnPropertyChanged(<@ this.DestinationAmountIssuer @>)
+        with get () = !destinationAmountIssuer
+        and set value = set this value destinationAmountIssuer <@ this.DestinationAmountIssuer @>
 
     member this.Submit() =
 
         let destinationAmount =
-            match destinationAmountIssued with
-            | true  ->
-                let amount = { Value = Decimal.Parse destinationAmountValue; Currency = destinationAmountCurrency; Issuer = destinationAmountIssuer }
-                IssuedAmount amount
-            | false ->
-                let amount = Decimal.Parse destinationAmountValue
-                NativeAmount amount
+            !destinationAmountIssued
+            |> toAmount !destinationAmountValue !destinationAmountCurrency !destinationAmountIssuer
 
         let command =
-            { SourceAccount = sourceAccount
-              DestinationAccount = destinationAccount
+            { SourceAccount = !sourceAccount
+              DestinationAccount = !destinationAccount
               DestinationAmount = destinationAmount }
 
         executeCommand (command |> RipplePathFind)
@@ -226,16 +204,14 @@ type TxModel() =
 
     inherit Model()
 
-    let mutable transaction = ""
+    let transaction = ref ""
 
     member this.Transaction
-        with get () = transaction
-        and set value =
-            transaction <- value
-            this.OnPropertyChanged(<@ this.Transaction @>)
+        with get () = !transaction
+        and set value = set this value transaction <@ this.Transaction @>
 
     member this.Submit() =
 
-        let command = { Transaction = transaction }
+        let command = { Transaction = !transaction }
 
         executeCommand (command |> Tx)
